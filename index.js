dc5153a8a96965ac67351e6c91593e4cd456885e
manish <manishkumar60708090@gmail.com>
2024-03-02 20:12:03 +0530

    how to make redux

---
+++ index.js
@@ -0,0 +1,12 @@
+import {createStore} from "redux"
+
+// how to make reducer?
+
+// store 
+const store = createStore();   // rudux is make a store 
+
+
+
+function reducer (state, action) {    // and reducer, reducer is return state
+    return state
+}
