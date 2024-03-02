bd545f3a6a839ead761fa14cb4d392253aea203e
manish <manishkumar60708090@gmail.com>
2024-03-02 20:41:40 +0530

    set the subscribe function update the every time in store

--- index.js
+++ index.js
@@ -9,23 +9,29 @@ const store = createStore(reducer);   // rudux is make a store
 // reducer
 function reducer (state={amount:1}, action) {    // state ke value aap initialize kar sakte ho, yani de sakte ho
     if(action.type === 'increment'){
-        // return {amount:state.amount+1}
+        return {amount:state.amount+1}
 
         // imutability the output 
-        state.amount = state.amount + 1
+        // state.amount = state.amount + 1
     }
     return state
 }
 
 
-// global state
-console.log(store.getState())   // state ko check karne ke liye ek function hota hai getState()
+
+// global state // this is subscribe function update the every time of store?
+store.subscribe(() => {   
+    console.log(store.getState())
+})
+
+
+
 
 
 // add action in redux
 store.dispatch({type:'increment'})   // state ke store me value add hogi
 
-console.log(store.getState())
+
 
 
 
