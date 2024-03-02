ad698d88d918773fed9fcaebac852c73d4110c89
manish <manishkumar60708090@gmail.com>
2024-03-02 20:31:04 +0530

    how to increment the value of state

--- Readme.md
+++ Readme.md
@@ -3,5 +3,6 @@ how to intilize the value of state commit output:
 ![alt text](<Screenshot 2024-03-02 202134.png>)
 
 
-
+how to increment the value of state, see the output:
+![alt text](<Screenshot 2024-03-02 202938.png>)
 
--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const store = createStore(reducer);   // rudux is make a store
 
 // reducer
 function reducer (state={amount:1}, action) {    // state ke value aap initialize kar sakte ho, yani de sakte ho
-    
+    if(action.type === 'increment'){
+        return {amount:state.amount+1}
+    }
     return state
 }
 
@@ -16,4 +18,14 @@ function reducer (state={amount:1}, action) {    // state ke value aap initializ
 // global state
 console.log(store.getState())   // state ko check karne ke liye ek function hota hai getState()
 
+
+// add action in redux
+store.dispatch({type:'increment'})   // state ke store me value add hogi
+
+console.log(store.getState())
+
+
+
+{ amount: 1 }   // this is initial state 
+{ amount: 2 }   // fintal state 
 
